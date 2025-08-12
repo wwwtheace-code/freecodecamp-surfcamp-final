ae1bd0a9cbb4e74d139038dde6aad829daf09f32
THEa <www.the.ace@gmail.com>
2025-08-12 11:18:47 +0400

    Update page.tsx
    Use Runtime Fetching

--- client/src/app/events/[slug]/page.tsx
+++ client/src/app/events/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import type { EventProps } from "@/types";
 import { ContentList } from "@/components/ContentList";
 import { Card, type CardProps } from "@/components/Card";
