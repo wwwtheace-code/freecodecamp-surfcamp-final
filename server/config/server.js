22eb58ba9651a58a03d076e635ac181ceb57a4f7
THEa <www.the.ace@gmail.com>
2025-08-13 15:40:59 +0400

    Delete server/config/server.js

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-console.log('APP_KEYS:', process.env.APP_KEYS);
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://152.42.244.6:1337'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
